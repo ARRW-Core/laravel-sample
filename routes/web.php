7fa40c125f298dc7df2cc8bee9632fd95b7bc926
ARRW-Core <raig.ramzy@gmail.com>
2022-09-01 09:40:13 +0500

    Another commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
